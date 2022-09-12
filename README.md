77db08efeacd34ce383e2373cd30787c117d48a1
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2022-09-12 14:29:12 +0300

    int

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+dev-server: foreman start -f Procfile.dev
---
+++ app/frontend/App.vue
@@ -0,0 +1,20 @@
+<template>
+  {{ message }}
+
+  <ButtonCounter />
+</template>
+
+<script>
+import ButtonCounter from "./components/ButtonCounter.vue";
+
+export default {
+  data() {
+    return {
+      message: "Hello Vue.js !",
+    };
+  },
+  components: {
+    ButtonCounter,
+  },
+};
+</script>
