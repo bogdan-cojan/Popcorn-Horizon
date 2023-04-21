1d102018c90404a447076b1449d3b1081e4b305e
bogdan <bogdan@bogdan.localdomain>
2023-04-21 11:24:42 +0300

    Movie show component

--- app/frontend/components/Movies.vue
+++ app/frontend/components/Movies.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="container-fluid row row-cols-1 row-cols-md-3 g-4">
     <div class="col" :key="movie.id" v-for="movie in this.movies">
-      <MovieCard :movie="movie" />
+      <MovieCard v-if="!this.movie" :movie="movie" @click="showMovie(movie)" />
     </div>
+  </div>
+  <div v-if="this.movie" class="container-fluid">
+    <div class="container-fluid" @click="this.movie=null">
+      <i style="color: green;font-size: 24px;" class="bi bi-arrow-left-circle-fill"></i>
+    </div>
+    <Movie :movie="this.movie" />
   </div>
 </template>
 
 <script>
 import MovieCard from "./MovieCard.vue";
 import { eventBus } from "../entrypoints/eventBus";
+import Movie from "./Movie.vue";
 
 export default {
   data() {
     return {
       movies: [],
+      movie: null,
     };
   },
   methods: {
+    showMovie(movie) {
+      this.movie = movie;
+    },
+
     async fetchMovies() {
       const res = await fetch("/apis/v1/movies");
       const data = await res.json();
@@ -31,6 +43,7 @@ export default {
   },
   components: {
     MovieCard,
+    Movie
   },
 };
 </script>
